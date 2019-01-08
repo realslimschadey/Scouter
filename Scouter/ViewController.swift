a9ee099eda40c2775d80531cd3106d4e5cfbca52
mxs3419 <mxs3419@g.rit.edu>
2019-01-08 17:12:53 -0500

    Initial Commit

---
+++ Scouter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scouter
+//
+//  Created by Michael Schade on 1/8/19.
+//  Copyright © 2019 Michael Schade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
